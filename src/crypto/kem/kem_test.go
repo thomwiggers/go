cd821f1442d6f72de151581125c2dcfa0f52f57a
Thom Wiggers <thom@thomwiggers.nl>
2020-07-21 14:47:24 +0200

    Allow for parameterisation of kem test

--- src/crypto/kem/kem_test.go
+++ src/crypto/kem/kem_test.go
@@ -7,17 +7,27 @@ import (
 )
 
 func TestKemAPI(t *testing.T) {
-	publicKey, privateKey, err := Keypair(rand.Reader, Kem25519)
-	if err != nil {
-		t.Error(err)
+	tests := []struct {
+		name string
+		kem  KemID
+	}{
+		{"Kem25519", Kem25519},
 	}
-	ss, ct, err := Encapsulate(rand.Reader, &publicKey)
-	if err != nil {
-		t.Error(err)
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			publicKey, privateKey, err := Keypair(rand.Reader, tt.kem)
+			if err != nil {
+				t.Error(err)
+			}
+			ss, ct, err := Encapsulate(rand.Reader, &publicKey)
+			if err != nil {
+				t.Error(err)
+			}
 
-	ss2, err := Decapsulate(&privateKey, ct)
-	if err != nil || !bytes.Equal(ss, ss2) {
-		t.FailNow()
+			ss2, err := Decapsulate(&privateKey, ct)
+			if err != nil || !bytes.Equal(ss, ss2) {
+				t.FailNow()
+			}
+		})
 	}
 }
