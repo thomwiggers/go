ad78e3f03ab68462cb9c9b7925759c1f6a50403e
Thom Wiggers <thom@thomwiggers.nl>
2020-07-21 14:57:55 +0200

    Improve test

--- src/crypto/kem/kem_test.go
+++ src/crypto/kem/kem_test.go
@@ -17,17 +17,27 @@ func TestKemAPI(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			publicKey, privateKey, err := Keypair(rand.Reader, tt.kem)
 			if err != nil {
-				t.Error(err)
+				t.Fatal(err)
 			}
 			ss, ct, err := Encapsulate(rand.Reader, &publicKey)
 			if err != nil {
-				t.Error(err)
+				t.Fatal(err)
 			}
 
 			ss2, err := Decapsulate(&privateKey, ct)
-			if err != nil || !bytes.Equal(ss, ss2) {
-				t.FailNow()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(ss, ss2) {
+				t.Fatal("Decapsulated differing shared secret")
 			}
 		})
 	}
+
+	// check if nonexisting kem fails
+	fakeKem := KemID(0)
+	if _, _, err := Keypair(rand.Reader, fakeKem); err == nil {
+		t.Fatal("This KEM should've been invalid and failed")
+	}
+
 }
